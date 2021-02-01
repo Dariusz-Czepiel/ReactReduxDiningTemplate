ffc4b7758874f7e3c4ba662588d5aae5a05a6db8
dariusz.czepiel <dariusz.czepiel@protonmail.com>
2021-02-02 00:52:45 +0100

    Remade all the basic components into const functions with hooks

--- ReactReduxDiningTemplate/ClientApp/src/App.tsx
+++ ReactReduxDiningTemplate/ClientApp/src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-//import logo from './logo.svg';
+//import React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
@@ -18,22 +17,3 @@ function App() {
 }
 
 export default App;
-
-/*
-  <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
- */
