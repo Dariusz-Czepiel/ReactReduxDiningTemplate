c81f418e5a9abcd717c65443050e6481e6c281b6
dariusz.czepiel <dariusz.czepiel@protonmail.com>
2021-02-13 23:11:03 +0100

    Changed project name Removed old project Added new react identity project

---
+++ Net5_React_DiningTemplate.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Net5_React_DiningTemplate.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net5_React_DiningTemplate.Web.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
