b0adefda9abc4b8206b119186a3af7b80219896b
dariusz.czepiel <dariusz.czepiel@protonmail.com>
2021-02-14 17:43:04 +0100

    Add basic ONION structure

---
+++ Net5_React_DiningTemplate.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Net5_React_DiningTemplate.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
