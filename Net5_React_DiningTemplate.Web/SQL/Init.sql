2f18ba4f6fe256c5cf2c7e85e0d0454c90a93d3a
dariusz.czepiel <dariusz.czepiel@protonmail.com>
2021-02-14 15:06:03 +0100

    added more init sql init user roles

--- Net5_React_DiningTemplate.Web/SQL/Init.sql
+++ Net5_React_DiningTemplate.Web/SQL/Init.sql
@@ -1,3 +1,8 @@
-﻿--init roles
+﻿--init users?
+
+--init roles
 
 insert into [DiningTemplate].[dbo].[AspNetRoles](Id, [Name], NormalizedName) VALUES ('Admin', 'Admin', 'ADMIN')
+
+--init user roles
+insert into [DiningTemplate].[dbo].[AspNetUserRoles] VALUES ('33954827-b884-4bad-8514-46c2fded1a57', 'Admin')
